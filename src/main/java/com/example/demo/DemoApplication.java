1608902d633991b70e758160f02e862ad1656593
avinashmangaloresuresh <avinash.mangaloresuresh@sjsu.edu>
2022-11-05 02:10:57 -0700

    Basic datstore operation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+
+
+
+
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
@@ -16,6 +24,29 @@ public class DemoApplication {
 class HelloWorldController {
   @GetMapping("/")
   public String hello() {
-    return "hello world!";
+//    return "hello world!";
+	// Instantiates a client
+	    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+	    // The kind for the new entity
+	    String kind = "Task";
+	    // The name/ID for the new entity
+	    String name = "sampletask1";
+	    // The Cloud Datastore key for the new entity
+	    Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(name);
+
+	    // Prepares the new entity
+	    Entity task = Entity.newBuilder(taskKey).set("description", "Buy milk").build();
+
+	    // Saves the entity
+	    datastore.put(task);
+
+//	    System.out.printf("Saved %s: %s%n", task.getKey().getName(), task.getString("description"));
+
+	    // Retrieve entity
+	    Entity retrieved = datastore.get(taskKey);
+
+	    return "Retrieved "+taskKey.getName()+": "+ retrieved.getString("description");
+
   }
 }
