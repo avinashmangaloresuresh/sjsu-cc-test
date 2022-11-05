db3820a42eec2519f713dbeb7b44944009700932
avinashmangaloresuresh <avinash.mangaloresuresh@sjsu.edu>
2022-11-04 23:19:07 -0700

    Updated pom.xml file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DemoApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+// Add the controller.
+@RestController
+class HelloWorldController {
+  @GetMapping("/")
+  public String hello() {
+    return "hello world!";
+  }
 }
